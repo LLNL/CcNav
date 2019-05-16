61f6bf6f28be1bc78bc3431d45a29ce6f49d75eb
Pascal Aschwanden <aschwanden1@llnl.gov>
2019-05-16 15:48:23 -0700

    Submit file binding and load file setup

--- js/GetFileChoices.js
+++ js/GetFileChoices.js
@@ -21,7 +21,29 @@ OV.GetFileChoices = function() {
 
         var sel = '<select class="file_select">' + selects + '</select>';
 
-        ReusableView.modal({header: "Select a file", body: "files: " + sel })
+        ReusableView.modal({header: "Select a file", body: "files: " + sel });
+
+        $('.submit_file_choice').unbind('click').bind('click', submit_file_choice_);
+    };
+
+
+    var file_submitted_ = function( dat ) {
+
+        //  Load file contents into containers.
+        //  We'll need to modify loadFile such that it can handle the file
+        //  content loaded directly from file, rather than through the file loader.
+
+        loadFile( dat );
+    };
+
+    var submit_file_choice_ = function() {
+
+        Ajax.call({
+            url: 'ajax/SubmitFileChoices.cgi',
+            type: "GET",
+            success: file_submitted_,
+            error: file_submitted_
+        });
     };
 
     var get_ = function() {
