626e23915e3b748b3246d9c77a5bef377068aa65
Pascal Aschwanden <aschwanden1@llnl.gov>
2020-01-10 10:11:00 -0800

    put sample inputs and source code in sample_inputs

---
+++ sample_inputs/3/source0.c
@@ -0,0 +1,14 @@
+
+int source0() {
+  printf("i am source0\r\n");
+
+  int x;
+
+  for( x=0; x < 5; x++ ) {
+    loopme();
+  }
+}
+
+int loopme() {
+  printf("i am being looped\r\n");
+}
---
+++ sample_inputs/3/source2.c
@@ -0,0 +1,5 @@
+
+int source2() {
+  
+  printf("i am source2\r\n");
+}
