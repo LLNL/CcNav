408aaa72169e910bf11ed0fea43995282007fa47
Pascal Aschwanden <aschwanden1@llnl.gov>
2020-02-25 16:19:07 -0800

    first state manager

---
+++ js/StateManager.js
@@ -0,0 +1,17 @@
+OV.StateManager = function() {
+
+    var load_ = function() {
+
+    };
+
+    //  Run this every time the user moves window
+    var save_ = function() {
+
+    };
+
+    $(document).ready(load_);
+
+    return {
+        save: save_
+    }
+}();
