7a2967c482ac1c0e0799936f2d6a81fab7d922d7
Pascal Aschwanden <aschwanden1@llnl.gov>
2020-02-26 14:11:18 -0800

    add golden layout stateChanged handler and setting of state, as well as the reloading of state implementation.

--- js/StateManager.js
+++ js/StateManager.js
@@ -1,18 +1,34 @@
 OV.StateManager = function() {
 
+    var golden_layout_key_ = 'golden_layout_key';
+
     var load_ = function() {
 
+        var myLayout,
+            savedState = localStorage.getItem( golden_layout_key_ );
+
+        if( savedState !== null ) {
+            myLayout = new GoldenLayout( JSON.parse( savedState ) );
+        } else {
+            myLayout = new GoldenLayout( config );
+        }
+
+        bind_();
     };
 
 
     //  Run this every time the user moves window
-    var save_ = function() {
+    var bind_ = function() {
 
+        myLayout.on( 'stateChanged', function(){
+            var state = JSON.stringify( myLayout.toConfig() );
+            localStorage.setItem( golden_layout_key_, state );
+        });
     };
 
     $(document).ready(load_);
 
     return {
-        save: save_
+        bind: bind_
     }
 }();
