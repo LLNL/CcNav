be50af3c524920ae6e836a66efc84bf0ace050ce
Pascal Aschwanden <aschwanden1@llnl.gov>
2021-10-12 13:17:55 -0700

    set up files dir

--- static/CFGConf/main.js
+++ static/CFGConf/main.js
@@ -11,7 +11,7 @@ class Model {
 var cfgConfModel = new Model();
 var cfgView = new CFGView(cfgConfModel, "graphContainer", "right");
 
-var cfgConfPrefix = "cfgConf/static/CFGConf/files/";
+var cfgConfPrefix = "static/CFGConf/files/";
 // var cfgConfFile = "ltimes_cfg_conf_no_graph.json";
 // var cfgConfFile = "ltimes_cfg_conf_no_graph_lines171-181.json";
 // var cfgConfFile = "ltimes_cfg_conf_no_graph_lines226-235.json";
